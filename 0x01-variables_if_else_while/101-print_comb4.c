3678893fcbd4344a686c130d7b0527fbf9fac092
Cornato <dragonroar0099@gmail.com>
2023-09-09 00:16:38 +0300

    test

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - output differente combination
+ * Return: return 0 and exit
+ */
+int main(void)
+{
+	int aa;
+	int bb;
+	int cc;
+	int z = 0;
+
+	for (aa = '0'; aa <= '7'; aa++)
+	{
+		for (bb = '0'; bb <= '8'; bb++)
+		{
+			for (cc = '0'; cc <= '9'; c++)
+			{
+				if (aa < bb && bb <= cc && aa != bb && bb != cc)
+				{
+					putchar(aa);
+					putchar(bb);
+					putchar(cc);
+					z++;
+					if (z < 126)
+					{
+						putchar (',');
+						putchar (' ');
+					}
+				}
+			}
+		}
+		z++;
+	}
+	putchar('\n');
+	return (0);
+}
