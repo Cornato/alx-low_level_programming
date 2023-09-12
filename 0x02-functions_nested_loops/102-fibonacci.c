375298a7ce55e8ee384e235af8a56c8286197e2d
Cornato <dragonroar0099@gmail.com>
2023-09-13 00:08:12 +0300

    test

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	int num1, num2, Sum, Contuer;
+	long num1, num2, Sum, Contuer;
 
 	num1 = 0;
 	num2 = 1;
@@ -16,9 +16,9 @@ int main(void)
 		num1 = num2;
 		num2 = Sum;
 		if (Contuer != 49)
-			printf("%d, ", Sum);
+			printf("%ld, ", Sum);
 		else
-			printf("%d\n", Sum);
+			printf("%ld\n", Sum);
 	}
 	return (0);
 }
