e41bc907e2f8545de3b6ba2313d3810d1187d19e
Cornato <dragonroar0099@gmail.com>
2023-09-12 13:11:51 +0300

    test

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - lowercase character
+ *  * @l: The loweer to checked
+ *  * @u: The uppercase to checked
+ *
+ * Return: 1 if c lowercase else 0 otherwise
+ */
+int _isalpha(int l, int u)
+{
+	if (l >= 'a' && l <= 'z' || u >= 'A' && u <= 'Z')
+		return (1);
+	else
+		return (0);
+}
