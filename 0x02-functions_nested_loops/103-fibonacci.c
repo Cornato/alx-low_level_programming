f8d432745531a96972da530b7ec925a53ac6f5f9
Cornato <dragonroar0099@gmail.com>
2023-09-13 00:24:53 +0300

    test

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - 50 fibonacci numbers
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int num1 = 1;
+	int num2 = 2;
+	int num3 = 0;
+	long Sum;
+
+	while (num2 < 4000000)
+	{
+		if (num2 % 2 == 0)
+			num3 += num2;
+
+		Sum = num2;
+		num2 = num1;
+		num1 = Sum;
+			
+	}
+	printf("%ld\n", Sum);
+	return (0);
+}
