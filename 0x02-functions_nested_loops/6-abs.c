db66894d3ef6ec1499687cf01f3f8f0c951cbb7b
Cornato <dragonroar0099@gmail.com>
2023-09-12 13:28:48 +0300

    test

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - lowercase character
+ * @n: The _abs to checked
+ * Return: 1 if n greater than zero
+ */
+int _abs(int n)
+{
+	if (n > -1)
+		_putchar(n);
+	else if (n == 0)
+		_putchar(n);
+	else if (n == -98)
+		_putchar(n);
+	else
+		_putchar(n);
+}
