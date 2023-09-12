110f1c223dd830020c0f7eb73c545fc5bc1ffa15
Cornato <dragonroar0099@gmail.com>
2023-09-13 01:57:31 +0300

    test

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,43 @@
 
 int main(void)
 {
-	long int n1 = 1, n2 = 2, next;
-	int i;
+	int Counter, Checked1, Checked2;
+	long int Num1, Num2, Num3, Num4, Num5, Num6;
 
-	printf("%lu, %lu", n1, n2);
-
-	for (i = 2; i <= 98; i++)
+	Num1 = 1;
+	Num2 = 2;
+	Checked1 =  Checked2 = 1;
+	printf("%ld, %ld", Num1, Num2);
+	for (Counter = 0; Counter < 96; Counter++)
 	{
-	next = n1 + n2;
-	printf(", %lu", next);
-	n1 = n2;
-	n2 = next;
+		if (Checked1)
+		{
+			Num3 = Num1 + Num2;
+			printf(", %ld", Num3);
+			Num1 = Num2;
+			Num2 = Num3;
+		}
+		else
+		{
+			if (Checked2)
+			{
+				Num5 = Num1 % 1000000000;
+				Num6 = Num2 % 1000000000;
+				Num1 = Num1 / 1000000000;
+				Num2 = Num2 / 1000000000;
+				Checked2 = 0;
+			}
+			Num4 = (Num5 + Num6);
+			Num3 = Num1 + Num2 + (Num4 / 1000000000);
+			printf(", %ld", Num3);
+			printf("%ld", Num4 % 1000000000);
+			Num1 = Num2;
+			Num5 = Num6;
+			Num2 = Num3;
+			Num6 = (Num4 % 1000000000);
+		}
+		if (((Num1 + Num2) < 0) && Checked1 == 1)
+			Checked1 = 0;
 	}
 	printf("\n");
 	return (0);
