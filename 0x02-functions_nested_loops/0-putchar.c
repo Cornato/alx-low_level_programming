e5d3fa90154672179397fda3be36f624abc920f6
Cornato <dragonroar0099@gmail.com>
2023-09-12 12:22:37 +0300

    test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char Content[] = "_putchar\n";
+
+	for (i = 0; Content[i] != '\0'; i++)
+	{
+		_putchar(Content[i]);
+	}
+		
+	return (0);
+}
