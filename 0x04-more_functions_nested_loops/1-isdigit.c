e40557999c456215cc95d036fbea5b50492bd804
Cornato <dragonroar0099@gmail.com>
2023-09-14 12:38:06 +0300

    test

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,12 @@
 
 /**
  * _isdigit - Checker Number 1 - 9
- *  * @c: Checker Number true
+ * @c: Checker Number true
  * Return: 1 if c 1 - 9 else 0
  */
 int _isdigit(int c)
 {
-	if (c >= '1' && c <= '9')
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
