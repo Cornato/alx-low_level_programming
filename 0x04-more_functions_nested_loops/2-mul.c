9b80b49534161716705cfbc41acb35012eca1632
Cornato <dragonroar0099@gmail.com>
2023-09-14 12:41:47 +0300

    test

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Checker Number 1 - 9
+ * @a: Checker Number true
+ * @b: Checker Number true
+ * Return: 1 if c 1 - 9 else 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
