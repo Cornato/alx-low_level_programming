b5ac503ed073831b2a046c30d923b839e7bcec4b
Cornato <dragonroar0099@gmail.com>
2023-09-14 15:08:53 +0300

    test

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - return num without 2,4
+ *
+ * Return: Always
+ */
+void more_numbers(void)
+{
+	int i, b;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (b = 0; b < 14; b++)
+		{
+			if (b > 9)
+			{
+				_putchar('0' + b / 10);
+			}
+			if (b <= 14)
+			{
+				_putchar('0' + b % 10);
+			}
+		}
+		_putchar('\n');
+	}
+}
