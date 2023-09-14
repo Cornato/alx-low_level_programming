13f5b0cd2a074ad4b38930b888076a6b84a25c73
Cornato <dragonroar0099@gmail.com>
2023-09-14 13:26:45 +0300

    test

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,10 @@
  */
 void print_numbers(void)
 {
-	int num;
-
-	for (num = 0; num <= 9; num++)
-		_putchar('0' + num);
-
-	_putchar('\n');
+int num;
+for (num = 0; num <= 9; num++)
+{
+_putchar('0' + num);
+}
+_putchar('\n');
 }
