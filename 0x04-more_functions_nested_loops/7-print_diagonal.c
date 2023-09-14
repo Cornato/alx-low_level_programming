6430998c328cac15b2ca48412b433761fd206ee3
Cornato <dragonroar0099@gmail.com>
2023-09-14 15:27:02 +0300

    test

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - test
+ * @n: variable
+ * Return: ss
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, s;
+
+		for (i = 0; i < n; i++)
+		{
+			for (s = 0; s < i; s++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
