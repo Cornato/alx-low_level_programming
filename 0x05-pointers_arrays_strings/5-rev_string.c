10f7ce4f29420bf6dc7bf51a8e89aa6933c1f616
Cornato <dragonroar0099@gmail.com>
2023-09-18 19:51:16 +0300

    test

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,18 @@
 #include "main.h"
-#include <string.h>
-
 /**
- * rev_string - to rev_string value
- * @s: pointer
+ * rev_string - Reverse a value.
+ * @s: pointer.
  */
 void rev_string(char *s)
 {
 	int length = strlen(s);
 	int i, j;
-	char temp;
+	char oldVal;
 
 	for (i = 0, j = length - 1; i < j; i++, j--)
 	{
-	    
-	    temp = s[i];
-	    s[i] = s[j];
-	    s[j] = temp;
+		oldVal = s[i];
+		s[i] = s[j];
+		s[j] = oldVal;
 	}
 }
