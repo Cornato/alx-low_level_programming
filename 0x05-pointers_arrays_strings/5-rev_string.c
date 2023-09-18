52a17abcaa130730f4a61defcb7814916a6c9eea
Cornato <dragonroar0099@gmail.com>
2023-09-18 19:09:13 +0300

    test

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - to rev_string value
+ * @s: pointer
+ */
+void rev_string(char *s)
+{
+	int i;
+	int length = strlen(s);
+
+	for (i = length - 1; s[i] != '\0'; i--)
+		putchar(s[i]);
+
+	putchar('\n');
+}
