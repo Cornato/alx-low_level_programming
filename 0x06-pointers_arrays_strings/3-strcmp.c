563ee6bbdef490f909321418d904d45b8c7943df
Cornato <dragonroar0099@gmail.com>
2023-09-20 21:46:49 +0300

    test

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcmp - concatenates two strings
+ * @s1: char pointer
+ * @s2: char pointer
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	char Output = strcmp(s1, s2);
+
+	return (Output);
+}
