762dc81226e66b9412fa3760495510969b8e33a7
Cornato <dragonroar0099@gmail.com>
2023-09-20 23:18:45 +0300

    test

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,11 +6,11 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, j;
 	int len = n - 1;
 	int OldValue;
 
-	for (i = 0, len; i < len; i++, len--)
+	for (i = 0, j = len; i < j; i++, j--)
 	{
 		OldValue = a[i];
         	a[i] = a[len];
