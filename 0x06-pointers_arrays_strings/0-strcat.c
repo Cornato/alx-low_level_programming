f7135889700342b144022dd02ca523ca1b38cd16
Cornato <dragonroar0099@gmail.com>
2023-09-20 19:35:20 +0300

    test

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: char pointer
+ * @src: char pointer
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *Value = strcat(dest, src);
+
+	return (Value);
+}
