895c9b2e69c5649495ae9b66e97bbd139f5c568f
Cornato <dragonroar0099@gmail.com>
2023-09-20 19:40:52 +0300

    test

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings
+ * @dest: char pointer
+ * @src: char pointer
+ * @n: int variable
+ * Return: 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	int length = strlen(dest);
+
+	for (i = 0; src[i] != '\0' && n > 0; i++, n--, length++)
+	{
+		dest[length] = src[i];
+	}
+	return (dest);
+}
