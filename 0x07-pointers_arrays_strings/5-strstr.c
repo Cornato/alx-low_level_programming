dc7862cdaa4cae7a4a84ac27eef384b8a272e2f9
Cornato <dragonroar0099@gmail.com>
2023-09-25 17:05:57 +0300

    test

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *_strstr - concatenates two strings
+ * @haystack: char pointer
+ * @needle: char pointer
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *Value = _strstr(haystack, needle);
+
+	return (Value);
+}
