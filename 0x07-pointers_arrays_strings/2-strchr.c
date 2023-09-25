625e6444038c803e931d14c453d76a0c4312f1b1
Cornato <dragonroar0099@gmail.com>
2023-09-25 16:14:25 +0300

    test

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *_strchr - concatenates two strings
+ * @s: char pointer
+ * @c: char pointer
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	char *Value = strchr(s, c);
+	return (Value);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+int _putchar(char c);
+char *_strchr(char *s, char c);
+
+#endif /* MAIN_H */
