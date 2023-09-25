5a3438a09877f1d670e19639f7aa6cf2ce0345c9
Cornato <dragonroar0099@gmail.com>
2023-09-25 17:18:00 +0300

    test

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *print_chessboard - chessboard
+ * @a: char pointer
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int Player1, Player2;
+
+	for (Player1 = 0; Player1 < 8; Player1++)
+	{
+		for (Player2 = 0; Player2 < 8; Player2++)
+		{
+			_putchar(a[Player1][Player2]);
+		}
+	}
+	putchar('\n');
+}
