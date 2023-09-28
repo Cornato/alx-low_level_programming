75a0afc71131b0114e8f27741a5c7e0223b79c18
Cornato <dragonroar0099@gmail.com>
2023-09-28 12:15:13 +0300

    test

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
