21c3bddf55fe60932d943e15632c20ee96fd67b6
Cornato <dragonroar0099@gmail.com>
2023-09-28 12:39:00 +0300

    test

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,13 +8,14 @@
 void _print_rev_recursion(char *s)
 {
 
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		s++;
-		_print_rev_recursion(s);
-		_putchar(*s);
+		_putchar('\n');
+		return;
 	}
 	else
-		_putchar('\n');
-	
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
