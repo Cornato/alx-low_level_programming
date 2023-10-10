614427b600dd2ba075c5a148904b1f4688c59d81
Cornato <dragonroar0099@gmail.com>
2023-10-10 23:23:44 +0300

    test

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,40 @@
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * new_dog - create new dog
+ * @name: type char pointer
+ * @age: type float
+ * @owner: type char pointer
+ * Return: new struct dog else null
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dogsturct;
+
+	if (owner == NULL || name == NULL || age < 0)
+		return (NULL);
+	dogsturct = malloc(sizeof(dog_t));
+	if (dogsturct == NULL)
+	{
+		free(dogsturct);
+		return (NULL);
+	}
+	dogsturct->name = malloc(sizeof(char) * strlen(name) + 1);
+	if (dogsturct->name == NULL)
+	{
+		free(dogsturct);
+		return (NULL);
+	}
+	dogsturct->owner = malloc(sizeof(char) * strlen(owner) + 1);
+	if (dogsturct->owner == NULL)
+	{
+		free(dogsturct->name);
+		free(dogsturct);
+		return (NULL);
+	}
+	dogsturct->name = name;
+	dogsturct->age = age;
+	dogsturct->owner = owner;
+	return (dogsturct);
+}
