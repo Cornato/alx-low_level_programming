6d38e50d6f4803e9e8bbb1f86f032c5393dfb100
Cornato <dragonroar0099@gmail.com>
2023-10-27 00:10:22 +0200

    test

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
