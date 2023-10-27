1085f057bf51995e25eddc549d0fb530a5a6d654
Cornato <dragonroar0099@gmail.com>
2023-10-27 02:11:04 +0200

    test

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+/**
+ * flip_bits - bit at a given index
+ * @n: Pointer
+ * @m: Pointer
+ * Return: 1
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor_result = n ^ m;
+	unsigned int count = 0;
+
+	while (xor_result > 0)
+	{
+		count += xor_result & 1;
+		xor_result >>= 1;
+	}
+	return (count);
+}
